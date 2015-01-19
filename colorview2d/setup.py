7e12fb677a020ddd4740fc5a5a3f2906172741b4
Alois Dirnaichner <Alois.Dirnaichner@physik.uni-regensburg.de>
2015-01-19 18:18:08 +0100

    Updated setup.py and added a version history file.

--- colorview2d/setup.py
+++ colorview2d/setup.py
@@ -3,18 +3,17 @@
 from distutils.core import setup
 
 setup(name='colorview2d',
-      version='0.3',
+      version='0.4',
       description='2d color plotting tool',
       author='Alois Dirnaichner',
       author_email='alo.dir@gmail.com',
       url='https://gitorious.org/colorview2d',
       scripts=['colorview2d.py'],
-      py_modules=['gpfile','parser','floatslider','floatspin','toolbox'],
-      data_files=[('data',['demo.dat'])],
+      py_modules=['LinecutFrame','LineoutFrame','MainFrame','MyLine','PlotFrame','SlopeExFrame','Subject','View','CropFrame','LabelticksFrame','FloatValidator','gpfile','floatslider','floatspin','toolbox'],
+      data_files=[('data',['demo.dat']),('config',['default.config'])],
       requires=['wx',
                 'matplotlib',
                 'scipy',
                 'numpy',
-                'lmfit',
-                'skimage'])
+                'yaml'])
 
