92fbed966a5383e35d2464535d4fd92bd9f69e7d
Alois Dirnaichner <Alois.Dirnaichner@physik.uni-regensburg.de>
2015-01-19 20:20:27 +0100

    Applied the python packaging directory structure and updated the setup.py file.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 
 setup(name='colorview2d',
-      version='0.4',
+      version='beta0.4',
       description='2d color plotting tool',
       author='Alois Dirnaichner',
       author_email='alo.dir@gmail.com',
-      url='https://gitorious.org/colorview2d',
+      url='https://sourceforge.net/p/colorview2d',
       scripts=['colorview2d.py'],
-      py_modules=['LinecutFrame','LineoutFrame','MainFrame','MyLine','PlotFrame','SlopeExFrame','Subject','View','CropFrame','LabelticksFrame','FloatValidator','gpfile','floatslider','floatspin','toolbox'],
+      packages=['colorview2d'],
       data_files=[('data',['demo.dat']),('config',['default.config'])],
       requires=['wx',
                 'matplotlib',
