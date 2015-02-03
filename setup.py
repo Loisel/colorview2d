75c0700cadb3ef2fd9706e5fb632cb61ab9894bb
Alois Dirnaichner <Alois.Dirnaichner@physik.uni-regensburg.de>
2015-02-03 18:05:21 +0100

    Fixed the line in the MANIFEST.in file that led to the exclusion of the actual plugin files.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='colorview2d',
-      version='0.48',
+      version='0.49',
       description='2d color plotting tool',
       author='Alois Dirnaichner',
       author_email='alo.dir@gmail.com',
