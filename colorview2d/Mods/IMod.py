b607e8be39970bbb7dfe697f35f4b285a4213e15
Alois Dirnaichner <alodi@directbox.com>
2015-02-08 21:29:37 +0100

    Provided docs for the IMod interface module.

--- colorview2d/Mods/IMod.py
+++ colorview2d/Mods/IMod.py
@@ -4,9 +4,31 @@ import logging
 from yapsy.IPlugin import IPlugin
 import abc
 
+"""
+Interface to the mod plugin class.
+Specification of the minimum requirements of a plugin implementation.
+It also provides basic interaction with the ModWidget base class.
+In particular, the checkbox of the widget is handled.
+The following attributes are defined:
+
+  title (string): Title string of the plugin. Usually equal to the
+                  plugin/module name.
+  args (tuple): The arguments that have to be provided to the mod to work.
+  active (bool): The current state of the plugin.
+
+"""
+
 class IMod(IPlugin):
     __meta__ = abc.ABCMeta
+    """
+    The interface class is an abstract base class.
+    At present, none of the methods have to be overwritten, though.
+    """
     def __init__(self):
+        """
+        The init function should be called by the plugin implementation
+        to correctly initialize the title and provide logging.
+        """
         self.args = ()
         self.title = self.__class__.__name__
         logging.info('{} is initialized.'.format(self.title))
@@ -14,8 +36,17 @@ class IMod(IPlugin):
         
     def set_args(self,args):
         self.args = args
+    def get_args(self):
+        return self.args
 
     def activate(self):
+        """
+        Activate the plugin. Usually, this method does not have to be
+        overwritten by a plugin.
+        The yapsy plugin activation routine is called.
+        If a checkbox widget is defined, it is checked.
+        The mod is added to the pipeline.
+        """
         IPlugin.activate(self)
         logging.info("Activating mod {}.".format(self.title))
         self.active = True
@@ -24,6 +55,13 @@ class IMod(IPlugin):
         self.view.add_mod_to_pipeline(self.title,self.args)
         
     def deactivate(self):
+        """
+        Deactivate the plugin. Usually, this method does not have to be
+        overwritten by a plugin.
+        The yapsy plugin deactivation routine is called.
+        If a checkbox widget is defined, it is unchecked.
+        The mod is removed from the pipeline.
+        """
         IPlugin.deactivate(self)
         logging.info("Deactivating mod {}.".format(self.title))
         self.active = False
@@ -31,16 +69,19 @@ class IMod(IPlugin):
             self.widget.chk.SetValue(False)
         self.view.remove_mod_from_pipeline(self.title)
         
-    def get_args(self):
-        return self.args
 
     def register(self,view):
+        """
+        Register the mod with a view object. This method is not meant to
+        be overwritten.
+        """
         self.view = view
 
     def update_widget(self):
         """
-        Update the widget from the mod data.
-        The mod is activated by the update.
+        Update the widget using the mod data.
+        The mod is activated on call.
+        
         """
         self.active = True
         if hasattr(self,'widget'):
@@ -49,6 +90,16 @@ class IMod(IPlugin):
             logging.warning('Can not update widget: No widget created.')
 
     def create_widget(self,panel):
+        """
+        Create a widget for the plugin. This Method has to be overwritten
+        to create a custom widget, i.e., a widget that contains more
+        than a simple checkbox.
+
+        Args:
+          panel (wx.Panel): The panel object to create the widget on.
+        Returns:
+          widget (wx.ModWidget): The widget object.
+        """
         self.panel = panel
         self.widget = ModWidget(self,self.panel)
 
@@ -56,5 +107,10 @@ class IMod(IPlugin):
         
         
     def apply(self):
+        """
+        This method has to be overwritten to provide some useful
+        functionality.
+        It should modify the view object using the parameter in args.
+        """
         logging.warning('The apply method of the base class Mod should not be called directly.')
 
