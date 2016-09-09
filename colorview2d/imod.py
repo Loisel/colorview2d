4fe4f93c49659ca9c5615f7574bbd8133589bdd9
Alois Dirnaichner <alodi@users.sourceforge.net>
2016-09-09 13:14:41 +0200

    Cleaned module imod to comply with changes in cvfig. + signal handling is removed. + args attribute is removed. + a decorator is added to catch errors in the implementations of   IMod.apply.

--- colorview2d/imod.py
+++ colorview2d/imod.py
@@ -1,26 +1,17 @@
 
 """
-.. module:: imod
-
 IMod
 ----
 
 Interface to the mod plugin class.
 Specification of the minimum requirements of a plugin implementation.
-It also provides basic interaction with the :class:`ModWidget <colorview2d.ModWidget.ModWidget>` class.
-In particular, the checkbox of the widget is handled.
-
 """
 
 from modwidget import ModWidget
-from pydispatch import dispatcher
-import signal
 
 import logging
 from yapsy.IPlugin import IPlugin
 import abc
-
-# import view
 
 
 class IMod(IPlugin):
@@ -32,8 +23,7 @@ class IMod(IPlugin):
 
     :title (string): Title string of the plugin. Usually equal to the
                   plugin/module name.
-    :args (tuple): The arguments that have to be provided to the mod to work.
-    :active (bool): The current state of the plugin.
+    :default_args (tuple): A default set of arguments that works with the apply function.
     """
     __meta__ = abc.ABCMeta
     def __init__(self):
@@ -41,74 +31,29 @@ class IMod(IPlugin):
         The init function should be called by the plugin implementation
         to correctly initialize the title and provide logging.
         """
-        self.args = self.default_args = ()
+        self.default_args = ()
 
         self.title = self.__class__.__name__
-        logging.info('{} is initialized.'.format(self.title))
-        self.active = False
-        
-    def set_args(self,args):
-        self.args = args
-    def get_args(self):
-        return self.args
-
-    def activate(self):
-        """
-        Activate the plugin. Usually, this method does not have to be
-        overwritten by a plugin.
-        The yapsy plugin activation routine is called.
-        If a checkbox widget is defined, it is checked.
-        The mod is added to the pipeline.
-        """
-        IPlugin.activate(self)
-        logging.info("Activating mod {}.".format(self.title))
-        self.active = True
-        if hasattr(self,'widget'):
-            self.widget.chk.SetValue(True)
-
-        #view.add_mod_to_pipeline(self.title,self.args)
-        dispatcher.send(signal.FIG_ADD_MOD_TO_PIPELINE, self, modstring = self.title, modargs = self.args)
-        
-    def deactivate(self):
-        """
-        Deactivate the plugin. Usually, this method does not have to be
-        overwritten by a plugin.
-        The yapsy plugin deactivation routine is called.
-        If a checkbox widget is defined, it is unchecked.
-        The mod is removed from the pipeline.
-        """
-        IPlugin.deactivate(self)
-        logging.info("Deactivating mod {}.".format(self.title))
-        self.active = False
-        if hasattr(self,'widget'):
-            self.widget.chk.SetValue(False)
-
-        #view.remove_mod_from_pipeline(self.title)
-        dispatcher.send(signal.FIG_REMOVE_MOD_FROM_PIPELINE, self, modstring = self.title)
+        logging.info('Mod %s is initialized.' % self.title)
 
 
-    def reset(self):
+    @staticmethod
+    def handle_modfail(function):
+        """This is a decorator for the apply function in the mod implementations.
+        It is bound to the calling signature of the apply method.
+        This enforces a correct implementation of mod.apply(self, datafile, args)
         """
-        Reset the mod to default values.
-        """
-        self.args = self.default_args
-        self.active = False
-        if hasattr(self,'widget'):
-            self.widget.update()
-        
+        def decorated(self, datafile, args):
+            try:
+                function(self, datafile, args)
+                return True
+            except (ValueError, TypeError):
+                logging.warn('Mod %s failed. Args: %s' % (self.title, args))
+                return False
+        return decorated
 
-    def update_widget(self):
-        """
-        Update the widget using the mod data.
-        
-        """
-        self.active = True
-        if hasattr(self,'widget'):
-            self.widget.update()
-        else:
-            logging.warning('Can not update widget: No widget created.')
 
-    def create_widget(self,panel):
+    def create_widget(self, panel):
         """
         Create a widget for the plugin. This Method has to be overwritten
         to create a custom widget, i.e., a widget that contains more
@@ -128,7 +73,7 @@ class IMod(IPlugin):
 
         return self.widget
         
-        
+    @handle_modfail    
     def apply(self,datafile):
         """
         This method has to be overwritten to provide some useful
@@ -139,4 +84,18 @@ class IMod(IPlugin):
         :type datafiel: :class:`Gpfile <colorview2d.gpfile.Gpfile>`
         """
         logging.warning('The apply method of the base class Mod should not be called directly.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
