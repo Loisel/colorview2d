d296ccaa6746acc793cf5472f01c9826e5619a59
Alois Dirnaichner <alois.dirnaichner@physik.uni-regensburg.de>
2016-09-29 09:30:38 +0200

    Added version paramter to setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,20 @@
 
 from setuptools import setup
 
+VERSION = '0.6.1'
+
 setup(name='colorview2d',
-      version='0.6.1',
+      version=VERSION,
       description='2d color plotting tool',
       author='Alois Dirnaichner',
       author_email='alo.dir@gmail.com',
       url='https://github.com/Loisel/colorview2d',
-      download_url='https://github.com/Loisel/colorview2d/tarball/v0.6',
+      download_url='https://github.com/Loisel/colorview2d/tarball/v' + VERSION,
       packages=['colorview2d', 'test', 'colorview2d.mods'],
       package_data={'':['default.cv2d'], },
       include_package_data=True,
       install_requires=['pyyaml', 'scikit-image', 'matplotlib', 'numpy'],
       keywords=['plotting', 'colorplot', 'scientific', 'numpy', 'matplotlib'],
       classifiers=[],)
+
+
