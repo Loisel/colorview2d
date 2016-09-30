4838fc1d4238b007ea4536c1af6e5ba7d7811ca0
Alois Dirnaichner <alois.dirnaichner@physik.uni-regensburg.de>
2016-09-30 10:04:34 +0200

    Added package description to init.

--- colorview2d/__init__.py
+++ colorview2d/__init__.py
@@ -1,4 +1,26 @@
-from datafile import Datafile
-from cvfig import CvFig
-from imod import IMod
-import fileloaders
+"""
+*colorview2d* is a plotting tool intended to be used with
+scientific data (with dimensionful axes)
+with an easily extendable data modification (or filtering) toolbox.
+
+Tested with Python 2.7
+
+Dependencies
+------------
+numpy, scipy, matplotlib, pyyaml, scikit.image
+
+Homepage
+--------
+https://github.com/Loisel/colorview2d
+
+Copyright (c) 2016 Alois Dirnaichner <alo.dir@gmail.com>
+"""
+
+__author__ = "Alois Dirnaichner <alo.dir@gmail.com"
+__license__ = "GNU GPLv3"
+__version__ = "0.6.1"
+
+from colorview2d.datafile import Datafile
+from colorview2d.cvfig import CvFig
+from colorview2d.imod import IMod
+import colorview2d.fileloaders
